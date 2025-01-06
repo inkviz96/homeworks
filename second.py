e6316abf14219dae49f2502d62c543d775bb72dc
Biibars <tursunbekov.biybars@gmail.com>
2025-01-06 22:34:21 +0100

    'second'

---
+++ second.py
@@ -0,0 +1,37 @@
+# Создать переменную со значение "Baloon was red. But i like blue." 
+# с помощью imput получить строку и попытаться найти её в созданной
+# нами ранее переменой с помощью метода find. Без учёта регистра и отдельна с учётом регистра.
+
+# Создать 2 переменных которые будут получать значения функцией input.
+# Напечатать "Sum was <сумма длин обеих строк>" с помощью f строк
+
+# Используя функцию count найти самую часто встречающуюся букву в тексте
+# "I just returned from the greatest summer vacation! It was so fantastic, I never wanted it to end."
+
+#(1)
+j = 'Baloon was red. But i like blue.'
+k = input(': ')
+print(j.find(k))
+#(2)
+h = len(input())
+g = len(input())
+print(f"Sum was {h+g}")
+
+#(3)
+t = "I just returned from the greatest summer vacation! It was so fantastic, I never wanted it to end."
+t = t.lower()
+
+m_str = ''
+
+maxi = 0
+
+for a in set(t):
+
+    count = t.count(a)
+
+    if count > maxi and a != ' ':
+
+        m_str = a
+        maxi = count
+
+print(f"most letter = {m_str}, sum = {maxi}")
