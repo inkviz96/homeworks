9d1d678e4c1b6b3b3ebc7d038de3d33d6ad2054c
Mafrik <warkelov.kolya@icloud.com>
2025-01-13 21:34:11 +0100

    NikolaiMarkelov

--- HM1.py
+++ HM1.py
@@ -1,14 +1,14 @@
 
 name = input("Nikolai: ")
 surname = input("Markelov: ")
-print(f"Hello, {name} {surname}!")
+print(f"Hello, Nikolai Markelov !")
 text = "Baloon was red. But I like blue."
 search = input("search: ")
 print(f"Case sensitive: {text.find(search)}")
 print(f"Case insensitive: {text.lower().find(search.lower())}")
-str1 = input("First line: ")
-str2 = input("Second line: ")
-print(f"Sum of string lengths: {len(str1) + len(str2)}")
+Nikolai = input("First line: ")
+Markelov = input("Second line: ")
+print(f"Sum of string lengths: {len(Nikolai) + len(Markelov)}")
 text = "I just returned from the greatest summer vacation! It was so fantastic, I never wanted it to end."
 cleaned_text = text.replace(" ", "").lower()
 most_common = max(cleaned_text, key=cleaned_text.count)
